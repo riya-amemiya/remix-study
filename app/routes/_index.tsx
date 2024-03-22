92acc8506bbf778fbaa9dfd0dd8337743941c8ed
Riya Amemiya <riyaamemiya@gmail.com>
2024-03-22 23:46:12 +0900

    refactor(entry.server.tsx, root.tsx, routes/_index.tsx, package.json): サーバーコードのリファクタリング、スタイルの軽微な修正、依存関係の更新と新しい機能の追加を行いました

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/cloudflare";
 import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    {
+      name: "description",
+      content: "Welcome to Remix! Using Vite and Cloudflare!",
+    },
   ];
 };
 
