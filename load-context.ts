3fffc0e22031f69450986c2275b91efe6634f986
Riya Amemiya <riyaamemiya@gmail.com>
2024-03-22 23:52:55 +0900

    feat: アプリの言語設定を日本語に更新し、新しい依存関係を追加

--- load-context.ts
+++ load-context.ts
@@ -1,5 +1,6 @@
 import type { PlatformProxy } from "wrangler";
 
+// biome-ignore lint/correctness/noUndeclaredVariables: <explanation>
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
 declare module "@remix-run/cloudflare" {
