57bb4e6176ef803d19b59d362ab9ba94842d7150
Riya Amemiya <riyaamemiya@gmail.com>
2024-09-15 13:38:28 +0900

    style: eslint-disableコメントを追加

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,8 +1,7 @@
+/* eslint-disable */
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-// eslint-disable-next-line import/no-unresolved
 // biome-ignore lint/style/noNamespaceImport: <explanation>
 import * as build from "../build/server";
 
